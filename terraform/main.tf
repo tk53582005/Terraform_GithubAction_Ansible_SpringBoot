4c630580d83dc61d01dc9eddd3498e0ca5753c56
tk53582005 <tk53582005@yahoo.co.jp>
2025-12-04 13:37:20 +0900

    fix: allow SSH from any IP for GitHub Actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "web" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
